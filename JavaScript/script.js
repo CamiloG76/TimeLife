a834eee5b385d620cd145b6f073da0215e334eeb
CamiloG <juank27gm@gmail.com>
2026-09-09 00:42:27 -0500

    se realizaron algunos ajustes en los archivos .html y se completaron las 3 funcionalidades en java, se añadio calcularprecio y mostrarmensaje

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,6 +6,44 @@ function validarFormulario() {
     let paquete = document.getElementById("paquete").value;
 
     if (nombre == "" || correo == "" || destino == "" || personas == "" || paquete == "") {
-    alert("Completa todos los campos");
+        alert("Completa todos los campos");
+        return false;
+    }
+
+    return true;
+}
+///////////////////////////////////////////////////////////////////////////////////////////////
+function calcularPrecio() {
+    let personas = document.getElementById("personas").value;
+    let paquete = document.getElementById("paquete").value;
+    let precio = 0;
+
+    if (paquete == "basico") {
+        precio = 100;
+    }
+
+    if (paquete == "premium") {
+        precio = 200;
+    }
+
+    if (paquete == "temporal") {
+        precio = 300;
+    }
+
+    let total = personas * precio;
+
+    alert("El precio total es: $" + total);
+}
+////////////////////////////////////////////////////////////////////////////////
+function mostrarMensaje() {
+    alert("Reserva realizada correctamente");
+}
+
+function procesarReserva() {
+    if (validarFormulario()) {
+        calcularPrecio();
+        mostrarMensaje();
     }
 }
+
+
