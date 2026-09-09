8fedf2d1fddad7f47e48064c5e37c6dc36e9ddd7
CamiloG <juank27gm@gmail.com>
2026-09-08 22:40:16 -0500

    se hicieron algunas correciones de los estilos de las paginas y se realizo un adelanto en la carpeta JavaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,15 @@
+function validarFormulario() {
+    let nombre = document.getElementById("nombre").value;
+    let correo = document.getElementById("correo").value;
+    let destino = document.getElementById("destino").value;
+    let personas = document.getElementById("personas").value;
+    let paquete = document.getElementById("paquete").value;
+
+    if (nombre == "" || correo == "" || destino == "" || personas == "" || paquete == "") {
+    alert("Completa todos los campos");
+    }
+}
+
+function calcularPrecio() {
+
+}
